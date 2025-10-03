237bedade7914d84ca0389f1ea80c7a42b42b8ff
bogdantancic <b.tancic@gmail.com>
2025-10-03 12:56:45 +0200

    Update PublicProfilePage.jsx

--- client/src/pages/PublicProfilePage.jsx
+++ client/src/pages/PublicProfilePage.jsx
@@ -242,7 +242,7 @@ function PublicProfilePage() {
 
   return (
     // --- FIX IS HERE ---
-    <div className="min-h-screen bg-gray-50 flex justify-center p-4 sm:p-6 pt-28 sm:pt-32">
+    <div className="min-h-screen bg-gray-50 flex justify-center items-start sm:items-center p-4 pt-28 sm:p-6">
     {/* --- END OF FIX --- */}
       <div className="w-full max-w-md">
         {renderContent()}
