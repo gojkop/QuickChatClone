9c3950447afefe0a82fcd0ce6ed4666fcdf7d4f6
bogdantancic <b.tancic@gmail.com>
2025-10-03 15:48:57 +0200

    1

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -14,7 +14,26 @@ module.exports = {
         'brand-dark': '#111827',
         'brand-gray': '#4B5563',
         'brand-light-gray': '#F9FAFB',
-      }
+      },
+      animation: {
+        blob: "blob 7s infinite",
+      },
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+          "33%": {
+            transform: "translate(30px, -50px) scale(1.1)",
+          },
+          "66%": {
+            transform: "translate(-20px, 20px) scale(0.9)",
+          },
+          "100%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+        },
+      },
     },
   },
   plugins: [],
