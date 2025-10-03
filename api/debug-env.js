547dfb56b072e2f3caa8ef2de432bb5e515ed0cd
gojkop <gojkop@gmail.com>
2025-10-03 21:48:22 +0200

    Create debug-env.js

---
+++ api/debug-env.js
@@ -0,0 +1,10 @@
+export default function handler(req, res) {
+  return res.status(200).json({
+    hasAccountId: !!process.env.CLOUDFLARE_ACCOUNT_ID,
+    hasAccessKey: !!process.env.CLOUDFLARE_R2_ACCESS_KEY,
+    hasSecretKey: !!process.env.CLOUDFLARE_R2_SECRET_KEY,
+    hasBucket: !!process.env.CLOUDFLARE_R2_BUCKET,
+    hasPublicUrl: !!process.env.CLOUDFLARE_R2_PUBLIC_URL,
+    bucket: process.env.CLOUDFLARE_R2_BUCKET, // Show actual value to verify
+  });
+}
