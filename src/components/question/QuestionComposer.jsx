8c73b9969a98e7cbb235e7d584967bc68de3140d
bogdantancic <b.tancic@gmail.com>
2025-10-05 18:08:34 +0200

    Update QuestionComposer.jsx

--- src/components/question/QuestionComposer.jsx
+++ src/components/question/QuestionComposer.jsx
@@ -605,7 +605,7 @@ const QuestionComposer = forwardRef(({ onReady, hideButton = false }, ref) => {
                   </svg>
                   <div className="flex-1">
                     <p className="text-xs font-semibold text-blue-900 mb-1">
-                     Screen recording available on desktop
+                      Screen recording available on desktop
                     </p>
                     <p className="text-xs text-blue-700">
                       Use our desktop site to record your screen along with video and audio
