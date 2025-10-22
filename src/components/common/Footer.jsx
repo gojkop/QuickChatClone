758141dc8ddea018eb332d728be4448db00bbbdb
bogdantancic <b.tancic@gmail.com>
2025-10-22 16:12:01 +0200

    Update Footer.jsx

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,5 +1,5 @@
 // src/components/common/Footer.jsx
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useCookieConsent } from '@/context/CookieConsentContext';
 
